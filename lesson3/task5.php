fe00badfa43eb53c5e8ad1cde55dde64d6c9b1a9
AlexHotBogomol <Zakablukov777@gmail.com>
2019-04-26 17:55:59 +0300

    is_polindrome function

--- lesson3/task5.php
+++ lesson3/task5.php
@@ -25,3 +25,23 @@
 	echo "<br>";
 	echo min_max_in_arr([1, 2, -4, 5, 100], 'max');
 	echo "<hr>";
+
+    // Написать функцию для определение является ли строка палиндромом или нет. Функция должна возвращать булевое значение.
+    //  Палиндром это строка которая читается одинаково с конца как и с начала. Например "Мадам", "Шалаш", "Аргентина манит негра"
+    function is_palindrome(string $string){
+        $string = str_replace(' ', '', $string);
+        $string = strtolower($string);
+        $string_length = strlen($string);
+        for($i = 0; $i < $string_length/2; $i++){
+            if($string[$i] !=  $string[$string_length-1-$i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    echo is_palindrome("Argentina manit negra");
+    echo "<br>";
+    echo is_palindrome("madam");
+    echo "<br>";
+    echo is_palindrome("mad1am");
+    echo "<hr>";
