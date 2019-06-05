45a01b14ac84745f9819e7637f41083962e636eb
AlexHotBogomol <Zakablukov777@gmail.com>
2019-06-05 20:33:56 +0300

    task1 completed

---
+++ exam/task1.php
@@ -0,0 +1,28 @@
+<?php
+	/*Необходимо написать класс для подсчета и группировки слов в предложении.
+	Например: “Лето, наступило у нас лето”
+	Результат должен быть в виде массива:
+	[‘лето’] => 2,
+	[‘наступило’] => 1,
+	[‘у’] => 1,
+	[‘нас’] => 1 */
+
+	class StringCalculator{
+		const SEPARATORS = [",", ".", ";"];
+		static function calculateWords($string){
+			$string = mb_strtolower($string);
+			$words = explode(" ", $string);
+			$words = str_replace (self::SEPARATORS, "", $words);
+			$result = [];
+			foreach ($words as $word) {
+				if(!isset($result[$word])){
+					$result[$word] = 1;
+				}else{
+					$result[$word]++;
+				}
+			}
+			return $result;
+		}	
+	}
+
+	var_dump(StringCalculator::calculateWords("Лето, наступило у нас лето"));
