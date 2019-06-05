0a2207e0c387238831b1aff53ff9631bc48491b3
AlexHotBogomol <Zakablukov777@gmail.com>
2019-06-05 19:53:08 +0300

    task2 completed

---
+++ exam/task2.php
@@ -0,0 +1,17 @@
+<?php
+	/*Создайте класс и реализуйте в нем статический метод accumulate(array $numbers). Метод должен к каждому элементу массива применить функцию:
+				x = x * x
+	Пример: дано массив [1, 2, 3, 4, 5]
+	Результат должен быть в виде массива: [1, 4, 9, 16, 25]*/
+
+	class Changer{
+		static function accumulate(array $numbers){
+			return array_map(function($number){
+				return pow($number, 2);
+			}, $numbers); 
+		}
+	}
+
+	$array = [1, 2, 3, 4, 5];
+
+	var_dump(Changer::accumulate($array));
