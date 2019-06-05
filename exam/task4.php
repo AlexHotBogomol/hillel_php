5034d38167562e6eaab8ca3b8b770380f38cbec4
AlexHotBogomol <Zakablukov777@gmail.com>
2019-06-05 21:00:23 +0300

    task4 completed

---
+++ exam/task4.php
@@ -0,0 +1,24 @@
+<?php
+	/*Создайте класс и реализуйте метод который будет делать плоский массив из ассоциативного
+	Пример: [1,[2,3,null,4],[null],5]
+	Результат: [1,2,3,4,5]*/
+
+	$array = [1,[2,3,null,4],[null],5];
+	class ArrayConverter {
+		static function convert_array($array){
+			$result = [];
+			foreach ($array as $value) {
+				if(is_array($value)){
+					foreach ($value as $value_item) {
+						if(!is_null($value_item)){
+						 	$result[] = $value_item;
+						}
+					}
+				}else if(!is_null($value)){
+					$result[] = $value;
+				}
+			}
+			return $result;
+		}
+	}
+	var_dump(ArrayConverter::convert_array($array));
